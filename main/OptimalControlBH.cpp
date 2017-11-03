f3c7ba9e806cbf4e7502f2bfa9faee4f13877b48
fskovbo <f.skovbo@hotmail.com>
2017-11-03 15:39:04 +0100

    Added time evolution methods

---
+++ main/OptimalControlBH.cpp
@@ -0,0 +1,110 @@
+#include "itensor/all.h"
+#include "boson.h"
+#include "correlations.h"
+#include "TimeEvolve.hpp"
+#include <vector>
+#include <fstream>
+#include <ctime>
+
+using namespace itensor;
+using std::vector;
+
+int main()
+  {
+  int N = 5;
+  int Npart = 5;
+  int HilbertDim = 5;
+
+  auto sites = Boson(N,HilbertDim);
+
+  //
+  // Set the initial wavefunction matrix product state
+  //
+  auto state = InitState(sites);
+  int p = Npart;
+  for(int i = N; i >= 1; --i)
+      {
+      if (p >= 1) {
+        state.set(i,"Occ1");
+        p -= 1;
+      }
+      else
+          {
+          state.set(i,"Emp");
+          }
+      }
+
+  auto psi = IQMPS(state);
+
+  //
+  // Create Super-Fluid (SF) Hamiltonian using AutoMPO
+  //
+  double J = -1.0;
+  double U = 0;
+  double eps = 0;
+
+  auto ampo = AutoMPO(sites);
+  for(int i = 1; i < N; ++i) {
+    ampo += J,"A",i,"Adag",i+1;
+    ampo += J,"Adag",i,"A",i+1;
+  }
+  for (int i = 1; i <= N; ++i) {
+    ampo += U/2.0,"N(N-1)",i;
+    ampo += eps,"N",i;
+  }
+  auto H_SF = IQMPO(ampo);
+
+  //
+  // Set the parameters controlling the accuracy of the DMRG
+  //
+  auto sweeps = Sweeps(1);
+  sweeps.maxm() = 10,20,50,50,100,200;
+  sweeps.cutoff() = 1E-9;
+  sweeps.niter() = 2;
+  sweeps.noise() = 1E-7,1E-8,0.0;
+  println(sweeps);
+
+  //
+  // Begin the DMRG calculation
+  //
+  auto energy = dmrg(psi,H_SF,sweeps,"Quiet");
+  printf("Energy w.r.t. superfluid Hamiltonian: %f\n",energy);
+
+
+  //
+  // Setup time evolution with Mott-Insulator (MI) Hamiltonian
+  //
+  J = 0;
+  U = 1;
+
+  auto ampo2 = AutoMPO(sites);
+  for(int i = 1; i < N; ++i) {
+    ampo2 += J,"A",i,"Adag",i+1;
+    ampo2 += J,"Adag",i,"A",i+1;
+  }
+  for (int i = 1; i <= N; ++i) {
+    ampo2 += U/2.0,"N(N-1)",i;
+  }
+
+  auto args = Args("Cutoff=",1E-9,"Maxm=",50);
+  double dt = 1e-2;
+  auto ttotal = 1;
+  size_t Nsteps = ttotal/dt;
+
+
+  std::clock_t    start;
+  start = std::clock();
+
+    vector<AutoMPO> ampos;
+    ampos.reserve(Nsteps);
+    for (size_t i = 0; i < Nsteps; i++) {
+      ampos.push_back(ampo2);
+    }
+    vector<IQMPS> psi_t = TimeEvolve(psi, ampos, dt*Cplx_i, args);
+    vector<IQMPS> psi_t2 = TimeEvolveBack(psi_t.back(), ampos, dt*Cplx_i, args);
+
+  std::cout << "Time: " << (std::clock() - start) / (double)(CLOCKS_PER_SEC / 1000) << " ms" << std::endl;
+
+
+  return 0;
+  }
