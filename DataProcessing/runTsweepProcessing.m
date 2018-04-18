e6d0eb7a7155514e4230136f5c801be44a0ac7e1
fskovbo <f.skovbo@hotmail.com>
2018-04-18 17:29:11 +0200

    dummy commit

--- DataProcessing/runTsweepProcessing.m
+++ DataProcessing/runTsweepProcessing.m
@@ -1,12 +1,12 @@
 clear all; close all; clc;
 
-readDirectory = '../../../mnt/RandSeedN5/';
-writeDirectory = '../../DataProcessing/Plots/RandSeedN5/';
+readDirectory = '../../../mnt/LinSigSeedN5fix3/';
+writeDirectory = '../../DataProcessing/Plots/LinSigSeedN5fix3/';
 
 processStatus(readDirectory,writeDirectory);
 
 %processBestSolutions(readDirectory,writeDirectory)
-% processFidelityData(readDirectory,writeDirectory);
+processFidelityData(readDirectory,writeDirectory);
 
 
 % processCacheData(readDirectory,writeDirectory, 5.6);
