43067c94a23fcde8f59c8afdc64ff6505d88cfa0
fskovbo <f.skovbo@hotmail.com>
2018-04-25 15:38:22 +0200

    Added more cache

--- src/OCBoseHubbard_nlp.cpp
+++ src/OCBoseHubbard_nlp.cpp
@@ -204,6 +204,8 @@ bool OCBoseHubbard_nlp::intermediate_callback(AlgorithmMode mode,
   // save current control and cost to file
   if (cacheProgress) {
 
+    auto control = bControl.getCArray();
+
     std::ofstream outfile;
     std::string filename = "ProgressCache.txt";
     outfile.open(filename, std::ios_base::app);
@@ -212,8 +214,12 @@ bool OCBoseHubbard_nlp::intermediate_callback(AlgorithmMode mode,
       outfile << iter << "\t";
       outfile << obj_value << "\t";
       outfile << times.back() << "\t";
-      outfile << 2*(times.size()-1) + ls_trials*(times.size()-1) << "\n";
+      outfile << 2*(times.size()-1) + ls_trials*(times.size()-1) << "\t";
 
+      for (auto& val : control){
+        outfile << val << "\t";
+      }
+      outfile << "\n";
     }
     else std::cout << "Unable to open file\n";
 
